c04ace6081791f8d1baec97f2f354dfa1efd9ea6
Collin Chamberlin <144283889+CDChamberlin@users.noreply.github.com>
2024-08-17 12:12:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8000
 app.use(express.json())
 
 app.use('/', (req, res) =>{
-    res.send({status: 200, message: "Hello World 2" })
+    res.send({status: 200, message: "Hello World" })
 })
 
 app.listen(port, () => {
